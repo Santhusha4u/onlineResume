0ba32058f5e0c6448801dd85cad6d51337928845
Santhusha Alwis <santhusha4u@gmail.com>
2020-12-07 11:20:35 +0530

    Header, Footer components updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2020 Santhusha Alwis</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020</li>
+              <li>Design by <a title="Styleshout" href="http://www.santhushaalwis.me/">Santhusha Alwis</a></li>
            </ul>
 
         </div>
